8d22c604d1f3a319f33252d5c2e24c460be99a5e
kensplanet <kensplanet@yahoo.com>
2019-05-05 21:31:13 -0400

    Tests

--- src/test/java/com/kensplanet/familytreeapi/it/ExceptionIntegrationTest.java
+++ src/test/java/com/kensplanet/familytreeapi/it/ExceptionIntegrationTest.java
@@ -1,18 +1,14 @@
 package com.kensplanet.familytreeapi.it;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kensplanet.familytreeapi.model.Member;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
