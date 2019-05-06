ba3e75e20960549a9691c6e6da28a1781bc08730
kensplanet <kensplanet@yahoo.com>
2019-05-05 21:38:32 -0400

    Tests

--- src/main/java/com/kensplanet/familytreeapi/service/ParentService.java
+++ src/main/java/com/kensplanet/familytreeapi/service/ParentService.java
@@ -4,6 +4,8 @@ import com.kensplanet.familytreeapi.model.Parent;
 import com.kensplanet.familytreeapi.model.Member;
 import com.kensplanet.familytreeapi.repository.ParentRepository;
 import com.kensplanet.familytreeapi.repository.MemberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,8 @@ import java.util.List;
 @Transactional
 public class ParentService {
 
+    private final static Logger logger = LoggerFactory.getLogger(ParentService.class);
+
     @Autowired
     private MemberRepository memberRepository;
 
@@ -21,6 +25,7 @@ public class ParentService {
     private ParentRepository parentRepository;
 
     public Parent addCouple(Parent parentObject) {
+        logger.info("Saving couple {} in DB.", parentObject);
         Parent parent = new Parent();
         for (Member memberObject : parentObject.getMembers()) {
             Member member = memberRepository.findById(memberObject.getMemberId()).get();
@@ -31,6 +36,7 @@ public class ParentService {
     }
 
     public Member addParent(Member memberObject) {
+        logger.info("Saving parent {} in DB.", memberObject);
         Parent parent = parentRepository.findById(memberObject.getParent().getParentId()).get();
         Member member = memberRepository.findById(memberObject.getMemberId()).get();
         member.setParent(parent);
@@ -39,6 +45,7 @@ public class ParentService {
     }
 
     public List<Parent> getCouples() {
+        logger.info("Getting all couples from DB.");
         return parentRepository.findAll();
     }
 }
