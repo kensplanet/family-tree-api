4e1f5f8be926da3d116adea27abebde9f99118d1
kensplanet <kensplanet@yahoo.com>
2019-05-05 20:58:24 -0400

    Tests

--- src/main/java/com/kensplanet/familytreeapi/service/MemberService.java
+++ src/main/java/com/kensplanet/familytreeapi/service/MemberService.java
@@ -6,6 +6,7 @@ import com.kensplanet.familytreeapi.repository.MemberRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
@@ -38,7 +39,12 @@ public class MemberService {
 
     public Integer deleteMember(Integer memberId) {
         logger.info("Deleting member {} from DB.", memberId);
-        memberRepository.deleteById(memberId);
+        try {
+            memberRepository.deleteById(memberId);
+        } catch (EmptyResultDataAccessException emptyResultDataAccessException) {
+            logger.error("Member {} not found in DB.", memberId);
+            throw new MemberException("Member not found.");
+        }
         return memberId;
     }
 
--- src/test/java/com/kensplanet/familytreeapi/it/ExceptionIntegrationTest.java
+++ src/test/java/com/kensplanet/familytreeapi/it/ExceptionIntegrationTest.java
@@ -1,15 +1,19 @@
 package com.kensplanet.familytreeapi.it;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kensplanet.familytreeapi.model.Member;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
@@ -24,6 +28,20 @@ public class ExceptionIntegrationTest {
     @WithMockUser(username = "user", roles = "USER")
     public void gettingUnknownMemberIdThrows400() throws Exception {
         mockMvc.perform(get("/members/1000"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"USER", "ADMIN"})
+    public void deletingUnknownMemberIdThrows400() throws Exception {
+        mockMvc.perform(delete("/members/1000"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"USER", "ADMIN"})
+    public void deletingRelatedMemberIdThrows403() throws Exception {
+        mockMvc.perform(delete("/members/1000"))
                 .andExpect(status().isBadRequest());
     }
 }
