b8ce745b0e21a421335d812efe52b78f869a5bc1
kensplanet <kensplanet@yahoo.com>
2019-05-05 21:36:15 -0400

    Tests

--- src/main/java/com/kensplanet/familytreeapi/controller/FamilyTreeController.java
+++ src/main/java/com/kensplanet/familytreeapi/controller/FamilyTreeController.java
@@ -2,6 +2,8 @@ package com.kensplanet.familytreeapi.controller;
 
 import com.kensplanet.familytreeapi.model.Member;
 import com.kensplanet.familytreeapi.service.MemberService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,11 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/familyTree")
 public class FamilyTreeController {
 
+    private final static Logger logger = LoggerFactory.getLogger(FamilyTreeController.class);
+
     @Autowired
     private MemberService memberService;
 
     @RequestMapping(value = "/{personId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Member getMember(@PathVariable Integer personId) {
+        logger.info("Request for getting family tree {} received.", personId);
         return memberService.getMember(personId);
     }
 }
